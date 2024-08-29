72eb4a74a3bec6f5483e34b3459c570c39acc033
marchiore <marchiore.matheus@gmail.com>
2024-08-29 20:20:14 -0300

    feat: including SFML lib to the project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Pong desenvolvido em C++ 
+
+## Para compilar o programa executar:
+
+```
+g++ .\main.cpp -I SFML\include\ -L SFML\lib\ -lsfml-main -lsfml-graphics -lsfml-system -lsfml-window -o pong
+```
+
+## Para executar o arquivo compilado:
+```
+.\pong.exe
+```
+
+## Requisitos:
+
+Instalar o SFML e o compilador MinGW -> [Tutorial](https://terminalroot.com.br/2023/03/como-compilar-seus-jogos-em-sfml-com-gcc-mingw-no-windows.html)
--- main.cpp
+++ main.cpp
@@ -6,26 +6,26 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Pong the Game");
 
     // Carregar a fonte
-    sf::Font font;
+    // sf::Font font;
 
     // !font.loadFromFile("arial.ttf")
 
-    if (!font.loadFromFile("/System/Library/Fonts/Supplemental/Arial.ttf")) {
-        std::cout << "Erro ao carregar a fonte!" << std::endl;
-        return -1;
-    }
+    // if (!font.loadFromFile("arial.ttf")) {
+    //     std::cout << "Erro ao carregar a fonte!" << std::endl;
+    //     return -1;
+    // }
 
     // Criar o texto
-    sf::Text text;
-    text.setFont(font);
-    text.setString("Pong!");
-    text.setCharacterSize(50); // Tamanho da fonte
-    text.setFillColor(sf::Color::White); // Cor do texto
+    // sf::Text text;
+    // text.setFont(font);
+    // text.setString("Pong!");
+    // text.setCharacterSize(50); // Tamanho da fonte
+    // text.setFillColor(sf::Color::White); // Cor do texto
 
     // Centralizar o texto na tela
-    sf::FloatRect textRect = text.getLocalBounds();
-    text.setOrigin(textRect.width / 2.0f, textRect.height / 2.0f);
-    text.setPosition(sf::Vector2f(800 / 2.0f, 600 / 2.0f));
+    // sf::FloatRect textRect = text.getLocalBounds();
+    // text.setOrigin(textRect.width / 2.0f, textRect.height / 2.0f);
+    // text.setPosition(sf::Vector2f(800 / 2.0f, 600 / 2.0f));
 
     // Raquetes e bola
     sf::RectangleShape player1(sf::Vector2f(10, 100));
@@ -88,7 +88,7 @@ int main() {
         window.draw(player1);
         window.draw(player2);
         window.draw(ball);
-        window.draw(text); // Desenhar o texto na tela
+        // window.draw(text); // Desenhar o texto na tela
         window.display();
     }
 
